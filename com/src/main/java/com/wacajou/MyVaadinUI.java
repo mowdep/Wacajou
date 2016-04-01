35113a10e5dbd9af5bcb8607199f58e201a1dcbe
VolsungaPayraudeau <payraudeau.developpement@gmail.com>
2016-04-01 12:12:20 +0200

    Final

---
+++ com/src/main/java/com/wacajou/MyVaadinUI.java
@@ -0,0 +1,34 @@
+package com.wacajou;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.navigator.SpringViewProvider;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+@Theme("valo")
+@SpringUI
+public class MyVaadinUI extends UI{
+	
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		// TODO Auto-generated method stub
+		final VerticalLayout root = new VerticalLayout();
+		root.setSizeFull();
+		root.setMargin(true);
+		root.setSpacing(true);
+		setContent(root);
+		
+		
+	}
+
+}
