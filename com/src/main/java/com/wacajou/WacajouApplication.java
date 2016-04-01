0366bd8465c23107d792af2301b47a0e53a48909
VolsungaPayraudeau <payraudeau.developpement@gmail.com>
2016-04-01 11:25:52 +0200

    Project settings
    First step
    V 0.0.1

---
+++ com/src/main/java/com/wacajou/WacajouApplication.java
@@ -0,0 +1,12 @@
+package com.wacajou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WacajouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WacajouApplication.class, args);
+	}
+}
